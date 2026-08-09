772d68f0b96990059b289ea00329501e22ab5cb4
Enoch <enoch.07.w@gmail.com>
2026-08-09 23:01:57 +0530

    Fixed Schedule, Added Menu Bar and Required Actions, Can Change Schedule

--- Cli/console.py
+++ Cli/console.py
@@ -1,14 +1,16 @@
 # importing the required libraries
 from rich.console import Console
 from rich.table import Table
+from rich.panel import Panel
 import pandas as pd
 
 # Importing Roots
 from Path_mapper import SCHEDULE_CSV, DATA_ROOT
 
-# importing OCR_Extractor and schedule_pharaser modules for CSV
-from Schedule_Bot.OCR_Extractor import image_extractor
-from Schedule_Bot.schedule_pharaser import day_splitter, time_extractor
+# MOre imports
+from Schedule_Bot.OCR_Extractor import extract_table, corrupt_finder, corrupt_fixer, review_edit
+from Schedule_Bot.schedule_pharaser import csv_saver, text_counter
+from bot import load_schedule, query_handler
 
 # creating a console object
 console = Console()
@@ -31,9 +33,57 @@ def display_table(data):
     # printing the table to the console
     console.print(table)
 
+# linking up console.py with main.py to be used when user called!
+# this fuction used to choose what the user want, expanision of Schedule program
+def run_scheduler():
+    # print the options
+    console.print(Panel.fit(
+        "[bold blue]1[/bold blue]. View current schedule\n"
+        "[bold blue]2[/bold blue]. Upload new schedule\n"
+        "[bold blue]3[/bold blue]. Ask the bot\n"
+        "[bold blue]0[/bold blue]. Back to main menu",
+        title = "[Fox]: Schedule"
+    )
+    )
+    # letting user to choose
+    choice = input("\n[Fox]: Choose: ").strip()
+
+    # choice condition starts here
+    if choice == "1":
+        df = pd.read_csv(SCHEDULE_CSV)
+        display_table(df)
+    elif choice == "2":
+        # update user a hint?
+        console.print("[Fox]: Drag and Drop Works Too")
+        image_path = input("\n[Fox]: Path to new schedule image: ").strip()
+
+        # extracting the table
+        df = extract_table(image_path)
+        # find the corrupted details
+        df = corrupt_finder(df)
+        # fix the corrupted details
+        df = corrupt_fixer(df)
+        # let user review and edit the corrupted file
+        df = review_edit(df)
+
+        # reshape the long format
+        long_df = text_counter(df)
+        # save the file using saver function
+        csv_saver(long_df, SCHEDULE_CSV)
+        # update it to user
+        console.print("[Fox]: Schedule updated!.")
+    elif choice == "3":
+        # load the scheduler
+        df = load_schedule()
+        # ask Fox about the schedule 
+        keyword = input("[Fox]: Ask: ").strip()
+        # it displays the schedule
+        print(query_handler(keyword, df))
+
 if __name__ == "__main__":
     # reading the csv file
     # NEW - using your Path_mapper constant!
     data = pd.read_csv(SCHEDULE_CSV)
     # displaying the data in a table format
     display_table(data)
+    run_scheduler()
---
+++ main.py
@@ -0,0 +1,48 @@
+# importing nessary library
+from rich.console import Console
+from rich.panel import Panel
+from Cli.console import run_scheduler
+import pandas as pd
+from Path_mapper import SCHEDULE_CSV
+
+# adding console variable
+console = Console()
+
+# adding Menu Bar so it can be shown in the console!
+def menu_bar():
+    # adding options!
+    console.print(Panel.fit(
+    "[bold blue]1[/bold blue].Schedule\n"
+    "[bold blue]2[/bold blue].Finance\n"
+    "[bold blue]3[/bold blue].Haptics\n"
+    "[bold blue]4[/bold blue].Exit\n",
+    title = "[Fox]: Main Menu"))
+
+# this function let the user to choose and run program within menubar
+def run():
+    # adding loop to ask it repeatively
+    while True:
+        # showing the menu_bar
+        menu_bar()
+        # leting user to choose option
+        console.print("[Fox]: Enter option.....")
+        choice = input("\n<:=:>").strip()
+
+        # choice based execution
+        if choice == "1":
+            print("[Fox]: Entering Schedule")
+            run_scheduler()
+        elif choice == "2":
+            console.print("[Fox]: Finance module not built yet")
+        elif choice == "3":
+            console.print("[Fox]: Haptics module not built yet")
+        elif choice == "4":
+            console.print("[Fox]: See you Soon Boss.....")
+            break
+        else:
+            console.print("[Fox]: Invalid option, try again.")
+
+
+        
+if __name__ == "__main__":
+    run()
